2c776fca390f2071867b45860c6847d0de0b6cfc
Rizky Fauzan Hanif <contact@rizkyfauzan.id>
2024-12-07 09:18:59 +0000

    feat: initialize content and update configuration for new site

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,15 +8,15 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
-    pageTitleSuffix: "",
+    pageTitle: "rizkyfauzan.id",
+    pageTitleSuffix: " | Rizky Fauzan Hanif",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "rizkyfauzan.id",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     generateSocialImages: false,
