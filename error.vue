9697e66dabc122a3c0b8c0468db3467ebb9dea34
Fauzan <contact@rizkyfauzan.id>
2025-06-28 04:47:45 +0700

    feat: add error handling component with status display and navigation

---
+++ error.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import type { NuxtError } from '#app'
+
+const props = defineProps({
+    error: {
+        type: Object as () => NuxtError,
+        default: () => ({} as NuxtError)
+    }
+})
+
+const handleError = () => clearError({ redirect: '/' })
+</script>
+
+<template>
+    <div>
+        <p>Status code: {{ props.error?.statusCode }}</p>
+        <button @click="handleError">Back to home</button>
+    </div>
+</template>
