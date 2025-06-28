b7f53ce14d199db89743a08a1895789ea9888986
Fauzan <contact@rizkyfauzan.id>
2025-06-28 18:05:35 +0700

    refactor: restructure navigation bar layout for improved semantics and readability

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,7 @@
 <template>
     <div class="h-screen py-4 px-2 md:py-6 md:px-4 lg:py-6 lg:px-6 flex flex-col">
         <div class="flex-1 border rounded-xl border-border flex flex-col overflow-hidden relative">
-            <header class="px-6 py-4 bg-background border-b border-border flex-shrink-0">
-               <navigation-bar />
-            </header>
+            <navigation-bar />
             <main class="flex-1 overflow-hidden relative">
                 <div class="h-full overflow-y-auto scrollable-content relative">
                     <slot />
